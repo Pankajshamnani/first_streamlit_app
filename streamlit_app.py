1517e34aa8915d1b967a50351e2bb41b0a79f712
Pankaj Shamnani <48305112+Pankajshamnani@users.noreply.github.com>
2023-08-03 12:37:42 +0530

    using pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-streamlit.title('my first app')
-streamlit.header('Menu')
-streamlit.text('Idli')
-streamlit.text('wada')
+import pandas
+my_fruit_list=pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
